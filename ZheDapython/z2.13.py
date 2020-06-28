b03615d6ad8632091d69922f527687e116f94df1
hermione101 <hermione101@163.com>
2020-06-29 00:42:24 +0800

    第2章-13 分段计算居民水费

---
+++ ZheDapython/z2.13.py
@@ -0,0 +1,10 @@
+#coding=utf-8 
+#Version: python3.6.0 
+#Tools: Pycharm 2017.3.2 
+_author_ = ' Hermione'
+x=int(input())
+if x<=15:
+    print("{:.2f}".format((4*x)/3))
+else:
+    print("{:.2f}".format(2.5*x-17.5))
+
