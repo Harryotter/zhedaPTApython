75b7cce1936001246133ac5e2b0ef69c00ff66a5
hermione101 <hermione101@163.com>
2020-07-06 19:55:12 +0800

    第2章-8 转换函数使用 (30分)

---
+++ ZheDapython/z2.8.py
@@ -0,0 +1,10 @@
+#coding=utf-8 
+#Version: python3.6.0 
+#Tools: Pycharm 2017.3.2 
+_author_ = ' Hermione'
+m,n=input().split(",")
+n=eval(n)
+c=int(m,n)
+print(c)
+# eval(<字符串>)的作用是将输入的字符串变成 Python 语句，并执行该语句。
+# int(参1，参2) 当参一为字符串，参二为表示整数进制的字符串时，才可进行进制转换
