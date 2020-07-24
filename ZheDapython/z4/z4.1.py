6c3b80489ad2f1b8e43b5abada144390b461bba7
hermione101 <hermione101@163.com>
2020-07-24 08:56:03 +0800

    第4章-1 生成3的乘方表 (15分)

---
+++ ZheDapython/z4/z4.1.py
@@ -0,0 +1,8 @@
+#coding=utf-8 
+#Version: python3.6.0 
+#Tools: Pycharm 2017.3.2 
+_author_ = ' Hermione'
+n=int(input())
+for i in range(n+1):
+    print("pow(3,{}) = {}".format(i,pow(3,i)))
+# 本题不难，只需注意将x的y次方函数记清楚
