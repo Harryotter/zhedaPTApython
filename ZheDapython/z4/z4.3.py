2b0793c032a6a14acf0f75c757d7f7838159e6d4
hermione101 <hermione101@163.com>
2020-07-24 10:29:07 +0800

    第4章-3 猴子吃桃问题 (15分)

---
+++ ZheDapython/z4/z4.3.py
@@ -0,0 +1,10 @@
+#coding=utf-8 
+#Version: python3.6.0 
+#Tools: Pycharm 2017.3.2 
+_author_ = ' Hermione'
+n=int(input())
+res=1
+for i in range(n-1):
+    res=(res+1)*2
+print(res)
+# 本题型初次接触有点懵，其实就是求一定条件下的累加和
