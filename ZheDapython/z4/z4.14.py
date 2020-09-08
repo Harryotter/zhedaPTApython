bede8f0066a4d6e6b02be2d207d70aaf0fa74ff0
hermione101 <hermione101@163.com>
2020-09-08 20:49:37 +0800

    第4章-14 统计字符 (15分)

---
+++ ZheDapython/z4/z4.14.py
@@ -0,0 +1,27 @@
+#coding=utf-8 
+#Version: python3.6.0 
+#Tools: Pycharm 2017.3.2 
+_author_ = ' Hermione'
+count=0
+alp=0
+dig=0
+blank=0
+oth=0
+s=[]
+while True:
+    a=list(input())
+    count+=1
+    s.extend(a)
+    if len(s)+count>10:
+        count-=1
+        break
+for i in s:
+    if i.isalpha():
+        alp+=1
+    elif i.isdigit():
+        dig+=1
+    elif i.isspace():
+        blank+=1
+    else:
+        oth+=1
+print("letter = {}, blank = {}, digit = {}, other = {}".format(alp,blank,dig,oth))
